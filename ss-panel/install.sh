fa0f7c99bee431fc657ac43642af02f52a0840b7
柳啸虎 <liuxh3388@gmail.com>
2016-07-05 22:05:50 +0800

    Update install.sh

--- ss-panel/install.sh
+++ ss-panel/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -d libsodium-1.0.1 ]; then
+if [ ! -d libsodium-1.0.10 ]; then
     wget https://raw.githubusercontent.com/wxliuxh/shadowsocks_install/master/ss-panel/libsodium-1.0.10.tar.gz || exit 1
     tar xf libsodium-1.0.10.tar.gz || exit 1
 fi
